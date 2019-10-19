a86e32992f9e10ebe94cc51481b908eb88537d96
Phillipe Queiroz <phgqm7@gmail.com>
2019-10-18 23:34:12 -0300

    (DONE): Add class material

---
+++ material/gods.js
@@ -0,0 +1,42 @@
+exports.gods = [
+  {
+    firstname: 'Lucas',
+    lastname: 'Willamy'
+  },
+  {
+    firstname: 'Lailson',
+    lastname: 'Alves'
+  },
+  {
+    firstname: 'Victor',
+    lastname: 'Misael'
+  },
+  {
+    firstname: 'Tales',
+    lastname: 'Mateus'
+  },
+  {
+    firstname: 'João',
+    lastname: 'Araújo'
+  },
+  {
+    firstname: 'Gabriel',
+    lastname: ' Brilhante'
+  },
+  {
+    firstname: 'Phillipe',
+    lastname: 'César'
+  },
+  {
+    firstname: 'Felipe',
+    lastname: 'Keiler'
+  },
+  {
+    firstname: 'Lívia',
+    lastname: 'Belizário'
+  },
+  {
+    firstname: 'Davi',
+    lastname: 'Segundo'
+  }
+];
